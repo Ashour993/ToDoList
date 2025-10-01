3eb9f9db64472f19b48409466e12853ef3566b34
Ashour Malik Ismail <ashourmalikismail@MacBook-Air-som-tillhor-Ashour.local>
2025-10-01 18:07:11 +0200

    Jag har skapat toDo list, donList, Main

---
+++ toDoList.java
@@ -0,0 +1,22 @@
+public class toDoList {
+
+
+    public toDoList() {
+
+        JFrame frame = new JFrame ("To-Do List");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);    
+        frame.setVisible(true);
+
+
+
+
+
+
+
+    }
+
+
+    
+    
+}
