f7f3ed00fc4d11dde9e67c85241352c9ea81e1c1
Ashour Malik Ismail <ashourmalikismail@MacBook-Air-som-tillhor-Ashour.local>
2025-10-02 12:49:43 +0200

    Skapade lite knappar till todo och done

--- doneList.java
+++ doneList.java
@@ -1,47 +1,26 @@
-import java.awt.LayoutManager;
+import javax.swing.*;
+import java.awt.*;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollBar;
-import javax.swing.JScrollPane;
-import javax.swing.border.Border;
-import java.awt.BorderLayout;
-public class doneList {
-
-    public static DefaultListModel<String> doneListModel;
+public class DoneList {
+    public static DefaultListModel<String> doneListModel = new DefaultListModel<>();
     public static JList<String> doneList;
 
-    public doneList() {
-
-        doneListModel = new DefaultListModel<>();
+    public DoneList() {
         doneList = new JList<>(doneListModel);
 
-        JFrame frame = new JFrame ("Done List");
+        JFrame frame = new JFrame("Done List");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);    
+        frame.setSize(400, 300);
 
-        JPanel mainpanel = new JPanel(new BorderLayout());
-        JLabel donelabel = new JLabel("The List of completed tasks");
-        JScrollPane donescrollpane = new JScrollPane(doneList);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JLabel doneLabel = new JLabel("The List of Completed Tasks");
+        JScrollPane scrollPane = new JScrollPane(doneList);
 
-        mainpanel.add(donelabel,BorderLayout.NORTH);
-        mainpanel.add(donescrollpane, BorderLayout.CENTER);
-        frame.add(mainpanel);
+        mainPanel.add(doneLabel, BorderLayout.NORTH);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+        frame.add(mainPanel);
         frame.setLocation(1100, 100);
         frame.setVisible(true);
-
-        
-        
-        
-
-        
-
-
-
     }
-
-    
 }
--- toDoList.java
+++ toDoList.java
@@ -1,34 +1,77 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.DefaultListModel;
-import javax.swing.JList;
-import java.awt.BorderLayout;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class toDoList {
-   public static DefaultListModel<String> toDoListModel;
+public class ToDoList {
+    public static DefaultListModel<String> toDoListModel = new DefaultListModel<>();
     public static JList<String> toDoList;
+    public static JTextField taskField;
+    public static JButton addButton;
+    public static JButton doneButton;
+    public static DoneList doneListWindow;
 
-
-    public toDoList() {
-        toDoListModel = new DefaultListModel<>();
+    public ToDoList() {
         toDoList = new JList<>(toDoListModel);
 
         JFrame frame = new JFrame("To-Do List");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        frame.setSize(400, 400);
 
-        JPanel mainpanel = new JPanel(new BorderLayout());
-        JLabel toDolabel = new JLabel("The List of To-Do tasks");
-        JScrollPane toDoscrollpane = new JScrollPane(toDoList);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JLabel toDoLabel = new JLabel("The List of To-Do Tasks");
+        JScrollPane scrollPane = new JScrollPane(toDoList);
 
-        mainpanel.add(toDolabel, BorderLayout.NORTH);
-        mainpanel.add(toDoscrollpane, BorderLayout.CENTER);
-        frame.add(mainpanel);
-        frame.setLocation(1100, 100);
+        taskField = new JTextField();
+        addButton = new JButton("Add Task");
+        doneButton = new JButton("Send to Done");
+
+        JPanel inputPanel = new JPanel(new BorderLayout());
+        inputPanel.add(taskField, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
+        buttonPanel.add(addButton);
+        buttonPanel.add(doneButton);
+        inputPanel.add(buttonPanel, BorderLayout.EAST);
+
+        mainPanel.add(toDoLabel, BorderLayout.NORTH);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+        mainPanel.add(inputPanel, BorderLayout.SOUTH);
+
+        frame.add(mainPanel);
+        frame.setLocation(100, 100);
         frame.setVisible(true);
 
+        // Lägg till uppgift i To-Do List
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String task = taskField.getText().trim();
+                if (!task.isEmpty()) {
+                    toDoListModel.addElement(task);
+                    taskField.setText("");
+                }
+            }
+        });
+
+        // Skicka markerad uppgift till Done List
+        doneButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (doneListWindow == null) {
+                    doneListWindow = new DoneList();
+                }
+
+                java.util.List<String> selectedTasks = toDoList.getSelectedValuesList();
+                for (String task : selectedTasks) {
+                    DoneList.doneListModel.addElement(task);
+                    toDoListModel.removeElement(task);
+                }
+            }
+        });
     }
 
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new ToDoList());
+    }
 }
